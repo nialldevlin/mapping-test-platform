2235697521f93746d272bc4ed600808f9efba621
goliath <niallcdevlin@gmail.com>
2023-09-04 21:08:55 -0600

    stuff

--- catkin_ws/src/image/src/image_node.cpp
+++ catkin_ws/src/image/src/image_node.cpp
@@ -1,17 +1,22 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 image_transport::Publisher pub;
 
-void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-  pub.publish(msg);
+  cv_bridge::CvImagePtr cv_ptr;
+  cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+  pub.publish(cv_ptr->toImageMsg());
 }
 
-int main(int arc, char** argv)
+int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "image_node");
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw", 1, imageCallback);
-  pub = it.advertise("/gray_image", 1); 
+  pub = it.advertise("/gray_image", 1);
+  ros::spin();
 }
