04702817be95ea48c7675364e88ab8a5898e57c5
goliath <niallcdevlin@gmail.com>
2023-09-04 20:18:12 -0600

    stuff

--- catkin_ws/src/image/src/image_node.cpp
+++ catkin_ws/src/image/src/image_node.cpp
@@ -1,12 +1,15 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
 {
-
+  pub.publish(msg);
 }
 
-ros::NodeHandle nh;
-image_transport::ImageTransport it(nh);
-image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw", 1, imageCallback);
-image_transport::Publisher pub = it.advertise("/gray_image", 1);
+int main(int arc, char** argv)
+{
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw", 1, imageCallback);
+  image_transport::Publisher pub = it.advertise("/gray_image", 1); 
+}
