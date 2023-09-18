bbbaeaaec698a389efd17b8e29164b2f5c29bfb1
Niall Devlin <niallcdevlin@gmail.com>
2023-09-18 17:32:45 -0600

    added scripts

--- scripts/env_ctrl.sh
+++ scripts/env_ctrl.sh
@@ -1,5 +1,8 @@
 _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+shell=$(echo $SHELL | cut -d'/' -f 4)
 source ${_dir}/builder.sh
+source /opt/ros/noetic/setup.$shell
+source ${_dir}/../catkin_ws/devel/setup.$shell
 
 export ROS_MASTER_URI="http://$(getip goliath):11311"
 
