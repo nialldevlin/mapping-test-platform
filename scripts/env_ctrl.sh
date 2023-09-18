b52b3030af12c49c51821fe431e976706f32e1e1
Niall Devlin <niallcdevlin@gmail.com>
2023-09-18 17:29:02 -0600

    added scripts

--- scripts/env_ctrl.sh
+++ scripts/env_ctrl.sh
@@ -2,3 +2,5 @@ _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${_dir}/builder.sh
 
 export ROS_MASTER_URI="http://$(getip goliath):11311"
+
+alias view_image="rosrun image_view image_view image:=/gray_image _image_transport:=theora"
